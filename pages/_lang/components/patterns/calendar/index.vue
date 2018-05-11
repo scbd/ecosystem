1b04d1857c995ed5b06ee955cb9828fcd115f7e3
Randy Houlahan <randy.houlahan@cbd.int>
2018-05-10 20:53:14 -0400

    documentation for component

---
+++ pages/_lang/components/patterns/calendar/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section >
+
+    <Calendar/>
+
+  </section>
+</template>
+
+<script>
+import Calendar from '~/components/patterns/calendar/Calendar'
+export default {
+  name:'ComponentPatternsCalendarPage',
+  components:{Calendar}
+}
+</script>
+
+<style >
+
+</style>
+<style module>
+
+</style>
