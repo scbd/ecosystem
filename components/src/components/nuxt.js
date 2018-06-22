09884575276a3bdee44ecf60a0000cc03e8eec75
Randy Houlahan <randy.houlahan@cbd.int>
2018-06-22 14:19:56 -0400

    temp auth spot

---
+++ components/src/components/nuxt.js
@@ -0,0 +1,7 @@
+import Vue  from 'vue'
+import AuthIFrame from './components/AuthIFrame'
+const AuthIFrameClass = Vue.extend(AuthIFrame)
+
+export default async ({ app, store, serverStore }) => {
+
+}
