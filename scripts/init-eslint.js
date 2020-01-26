29243cde438a7dbe6116394d5e346bd43377cf54
Randy Houlahan <randy.houlahan@cbd.int>
2020-01-26 15:32:01 -0500

    save

--- scripts/init-eslint.js
+++ scripts/init-eslint.js
@@ -5,14 +5,11 @@ const packagesPath = path.resolve(__dirname, '../packages/')
 const esLintConfigFile = 'module.exports = ' + JSON.stringify(require('../.eslintrc'))
 const packageNames = readPackageNames()
 
-for (const packageName of packageNames) {
+for (const packageName of packageNames)
   fs.writeFileSync(`${packagesPath}/${packageName}/.eslintrc.js`, esLintConfigFile)
-}
-
-console.log('esLintConfigFile',packageNames)
 
 
 function readPackageNames(){
   return fs.readdirSync(packagesPath)
-  .filter((name) => name !== '.DS_Store' )
+    .filter((name) => name !== '.DS_Store')
 }
